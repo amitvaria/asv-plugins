aa51897bf1b438e7dd690801df0ac443940639a5
variaas <variaas@c64d0be3-9520-0410-ba41-dbcfa880427b>
2007-05-14 06:21:19 +0000

    created asv_list_plugins() to generate xml data for the pluginfeed

--- branches/asv_plugin_installer/asv_plugin_installer.php
+++ branches/asv_plugin_installer/asv_plugin_installer.php
@@ -19,7 +19,7 @@ $plugin['description'] = 'Adaption of Rob Sable\'s installer';
 // 0 = regular plugin; loaded on the public web side only
 // 1 = admin plugin; loaded on both the public and admin side
 // 2 = library; loaded only when include_plugin() or require_plugin() is called
-$plugin['type'] = 1; 
+$plugin['type'] = 0; 
 
 
 @include_once('zem_tpl.php');
@@ -34,15 +34,66 @@ h1. help!
 }
 
 # --- BEGIN PLUGIN CODE ---
-if(@txpinterface == 'admin') {
+/*if(@txpinterface == 'admin') {
 	add_privs('asv_plugin_installer','1,2');
 	register_tab('extensions', 'asv_plugin_installer', 'Plugin Installer');
 	register_callback('asv_plugin_installer', 'asv_plugin_installer');
+}*/
+
+function asv_plugin_installer($thing){
+	/*pagetop("Plugin Installer");
+	echo 
+	form(
+		startTable("list").n.t
+		.tr(
+			td(
+				hed("Plugin Installer", "1").n.t
+				.graf("Begin by adding plugin repositories. You can find repositories everywhere.").n
+			)
+		)
+		.tr(
+			td(
+				hed("Your Repositories", "2")
+				.graf("This will contain a list of repositories you have added")					
+			).
+			td(
+				htmlPre(asv_list_plugins())
+			)
+		)
+
+		.endTable()
+	);*/
+	
+	
 }
 
-function asv_plugin_installer($event, $step){
-	pagetop("Plugin Installer");
+function asv_list_plugins(){
+	global $file_base_path, $siteurl;
+	$out[] = '';
+	$rs = safe_rows_start('id, filename', 'txp_file', "1 order by filename");
 	
+	if ($rs and numRows($rs) > 0){
+		while($a = nextRow($rs)){
+			$asv_serialized = file_get_contents($file_base_path.'/'.$a['filename']);
+			$asv_serialized = preg_replace('@.*\$plugin=\'([\w=+/]+)\'.*@s', '$1', $asv_serialized);
+			$asv_serialized = preg_replace('/^#.*$/m', '', $asv_serialized);
+			if(trim($asv_serialized)) {
+				$asv_serialized = base64_decode($asv_serialized);
+				if (strncmp($asv_serialized,"\x1F\x8B",2)===0)
+					$asv_serialized = gzinflate(substr($asv_serialized, 10));
+		
+				if ($asv_serialized = unserialize($asv_serialized)) {
+					if(is_array($asv_serialized)){
+						extract($asv_serialized);
+					}
+				}
+			}
+			$out[]= "<item><title>".$name."</title><link>".$siteurl."/downloads/".$a['id']."</link><version>".$version."</version><description>".html_entity_decode($description)."</description></item>";
+		}
+	}
+	
+	
+	return join('', $out) ;
 }
 
 # --- END PLUGIN CODE ---
