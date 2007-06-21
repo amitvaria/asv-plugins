45ef164cdb63c66b45ac9aef0690cfceaa08b63b
variaas <variaas@c64d0be3-9520-0410-ba41-dbcfa880427b>
2007-06-21 01:00:56 +0000

    restructuring asv_breadcrumbs

--- trunk/asv_breadcrumb/asv_breadcrumbs.php
+++ trunk/asv_breadcrumb/asv_breadcrumbs.php
@@ -1,3 +1,39 @@
+<?php
+
+// This is a PLUGIN TEMPLATE.
+
+// Copy this file to a new name like abc_myplugin.php.  Edit the code, then
+// run this file at the command line to produce a plugin for distribution:
+// $ php abc_myplugin.php > abc_myplugin-0.1.txt
+
+// Plugin name is optional.  If unset, it will be extracted from the current
+// file name. Uncomment and edit this line to override:
+$plugin['name'] = 'asv_breadcrumbs';
+
+$plugin['version'] = '1.0';
+$plugin['author'] = 'Amit Varia';
+$plugin['author_uri'] = 'http://www.amitvaria.com/';
+$plugin['description'] = 'Adds breadcrumbs to TXP';
+
+// Plugin types:
+// 0 = regular plugin; loaded on the public web side only
+// 1 = admin plugin; loaded on both the public and admin side
+// 2 = library; loaded only when include_plugin() or require_plugin() is called
+$plugin['type'] = 0; 
+
+
+@include_once('zem_tpl.php');
+
+if (0) {
+?>
+# --- BEGIN PLUGIN HELP ---
+
+
+# --- END PLUGIN HELP ---
+<?php
+}
+
+# --- BEGIN PLUGIN CODE ---
 function asv_breadcrumb($atts)
 {
 	global $pretext,$thisarticle,$sitename;
@@ -71,3 +107,6 @@ function asv_breadcrumb($atts)
 	//Add article title without link if we're on an individual archive page?
 	return doTag(join($sep, $content), $wraptag, $class);
 }
+# --- END PLUGIN CODE ---
+
+?>
