71d4568e309469a97da5bfb00221e1dd4b53d5f0
variaas <variaas@c64d0be3-9520-0410-ba41-dbcfa880427b>
2007-06-22 06:32:27 +0000

    added basic form capabilities and animation

--- trunk/asv_amazon2/asv_amazon2.php
+++ trunk/asv_amazon2/asv_amazon2.php
@@ -10,7 +10,7 @@
 // file name. Uncomment and edit this line to override:
 $plugin['name'] = 'asv_amazon2';
 
-$plugin['version'] = '0.1';
+$plugin['version'] = '0.2';
 $plugin['author'] = 'Amit Varia';
 $plugin['author_uri'] = 'http://www.amitvaria.com/';
 $plugin['description'] = 'Add Amazon items to your TXP article';
@@ -38,6 +38,8 @@ _more help to come_
 # --- BEGIN PLUGIN CODE ---
 // asv_amazon2
 // Designed around the need to include items from Amazon in your website
+//-------------------------------------------------------------
+
 if('admin' == @txpinterface) 
 {
 	register_callback('asv_amazon2', 'article');
@@ -51,6 +53,7 @@ if(gps('asv_amazon2_action') && gps('asv_keywords'))
 	exit;
 }
 
+//-------------------------------------------------------------
 
 function asv_amazon2 ($event, $step)
 {
@@ -58,23 +61,34 @@ function asv_amazon2 ($event, $step)
 
 	$line = "<h3 class=\"plain\">";
 	
-	$line .= "<a href=\"#asv_amazon2\" onclick=\"toggleDisplay('asv_amazon2wrapper'); return false;\">Amazon2</a>";
+	$line .= "<a href=\"#asv_amazon2\" onclick=\"$('#asv_amazon2wrapper').slideToggle('slow'); return false;\">Amazon2</a>";
 
 	$line .= "</h3>";
 
 	$form = "<form onSubmit=\"asv_loadResults(); return false;\"><fieldset><legend>Search</legend>".
+	
 		graf("<label for=\"asv_SearchIndex\">Choose a category</label><br />".
+		
 			selectInput('asv_SearchIndex', $asv_searchIndex, $asv_searchIndex, true,'','asv_SearchIndex')).
+				
 		graf("<label for=\"asv_Keywords\">Keywords</label><br />".
+		
 			fInput('text', 'asv_Keywords', '', 'edit', '', '', '20',  '', 'asv_Keywords')).
+			
 		fInput('submit','asv_Search','Search',"publish", '', 'asv_loadResults();return false;', '', '').
+			
 		fInput('button','asv_Cancel','Cancel',"publish", '', 'asv_cancelResults();return false;', '', '').
+		
 		"</fieldset></form>";
-	$line .= "<div id=\"asv_amazon2wrapper\" style=\"display: none;\"><div id=\"asv_amazon2\">$form</div></div>";
+		
+	$line .= "<div id=\"asv_amazon2wrapper\" style=\"display:none\" ><div id=\"asv_amazon2\" >$form</div><div id=\"asv_amazon2form\" style=\"display:none\"></div><div id=\"asv_amazon2Results\" style=\"display:none\"></div></div>";
 	
 	$line = asv_safeJS($line);
 	
-		
+	$forms = array();
+	
+	$forms[0] = asv_safeJS('<txp:asv_amazon2 asin="[asv_amazon2_asin]"><txp:asv_amazon2_title /><txp:asv_amazon2_imageURL url="[asv_amazon2_imgURLm]" /><txp:asv_amazon2_url /></txp:asv_amazon2>');
+	
 	$js = <<<EOF
 <SCRIPT LANGUAGE="JavaScript" SRC="/textpattern/jquery.js">
 </SCRIPT>
@@ -82,18 +96,11 @@ function asv_amazon2 ($event, $step)
 <!--
 
 //------------------------------------------------------------- 
+//Attach the amazon2 plugin to the page
 
  $(document).ready(function() {
 	$("$line").insertBefore("#write-status");
    });
-
-//-------------------------------------------------------------  
-
-function asv_onClick(elem)
-{
-	var dest = document.getElementById('body');
-	dest.value += " " + elem;
-}
 
 //-------------------------------------------------------------
 
@@ -107,23 +114,17 @@ function asv_loadResults()
 //-------------------------------------------------------------
 
 function asv_request(keywords, searchindex, itempage){
-
-	$('#asv_amazon2Results').remove();
-	$('<div id="asv_amazon2Results"><fieldset id="asv_amazon2resultsFieldset"><legend>Results</legend><div id="asv_amazon2ResultsData"><p >loading...</p></div></fieldset></div>').appendTo("#asv_amazon2wrapper");
+	$('#asv_amazon2form').html('<fieldset><legend>Choose Form</legend><form><select name="amazonForm"><option>default</option></select></form>');
+	$('#asv_amazon2Results').html('<fieldset><legend>Results</legend><div id="asv_amazon2ResultsData"><p >loading...</p></div></fieldset>');
 	$("#asv_amazon2ResultsData").css("padding", "0px 0px 10px 0px");
+	$('#asv_amazon2form').show('slow');
+	$('#asv_amazon2Results').show('slow');
 	
 	$.get('index.php',
 	 	{asv_amazon2_action: "1", asv_keywords: escape(keywords), asv_searchindex: escape(searchindex), asv_itempage: itempage },
 	   asv_parseResponse,
 	   "xml"
 	 );
-}
-
-//-------------------------------------------------------------
-
-function asv_cancelResults()
-{
-	$('#asv_amazon2Results').remove();
 }
 
 //-------------------------------------------------------------
@@ -140,57 +141,141 @@ function asv_parseResponse(xml)
 
 	$(xml).find('Item').each(function(){
 		var title = $('Title',this).text();
-		var imageURL = $("SmallImage > URL",this).text();
+		var sImageURL = $("SmallImage > URL",this).text();
+		var mImageURL = $("MediumImage > URL",this).text();
+		var lImageURL = $("LargeImage > URL",this).text();
 		var url = $('DetailPageURL',this).text();
+		var asin = $('ASIN',this).text();
 		
-		var amazonHTML = '<a href="'+url+'"><img src="'+imageURL+'" style="display: block;margin-left: auto;margin-right: auto;"/><span style="text-align: center">' + title + '</span></a>' ;
 		
-		var click ='<a href="#" onclick="asv_onClick(\'' + title+ '\');return false;">add</a>';
-		line += '<p>' + amazonHTML + '<hr style="padding: 0px; margin: 0px; height: 1px; color: #000;" />' + click + '</p>';
+		var amazonHTML = '<a href="'+url+'"><img src="'+ sImageURL +'" style="display: block;margin-left: auto;margin-right: auto;"/><span style="text-align: center">' + title + '</span></a>' ;
+		
+		var form = '<form id="asv_amazon2_'+asin+' onsubmit="asv_amazon2_addtoBody(\''+asin+'\')">' + 
+					'<input type="hidden" name="asv_amazon2_title" value="'+title+'" />' +
+					'<input type="hidden" name="asv_amazon2_sImageURL" value ="'+sImageURL+'" />' +
+					'<input type="hidden" name="asv_amazon2_mImageURL" value ="'+mImageURL+'" />' +
+					'<input type="hidden" name="asv_amazon2_lImageURL" value ="'+lImageURL+'" />' +
+					'<input type="hidden" name="asv_amazon2_url" value ="'+url+'" />' +
+					'<input type="hidden" name="asv_amazon2_asin" value ="'+asin+'" />' +
+					'<input type="button" style="float:right" onclick="asv_amazon2_addtoBody(\''+asin+'\')" class="publish" value="add" />'
+				   '</form>';
+		
+		line += '<p>' + form + amazonHTML  + '</p>';
     });
 
 	line += asv_prevnext_link(keywords, searchindex, itemPage, totalPages, "bottom");
     
-    $("#asv_amazon2ResultsData").remove();
-    $('<div id="asv_amazon2ResultsData">' + line + '</div>').appendTo("#asv_amazon2resultsFieldset");
+    $("#asv_amazon2ResultsData").html(line);
 	$("#asv_amazon2ResultsData").css("max-height", "500px");
 	$("#asv_amazon2ResultsData").css("overflow", "auto");
 	$("#asv_amazon2ResultsData").css("padding", "0px 0px 10px 0px");
 }
 
+//-------------------------------------------------------------  
+
+$.fn.appendVal = function(txt) {
+    return this.each(function(){
+        this.value += txt;
+    });
+
+}; 
+
+//-------------------------------------------------------------  
+
+function asv_amazon2_addtoBody(asin)
+{
+	var elem = $('#asv_amazon2_'+asin);
+	
+	var title = $('input:hidden[@name=asv_amazon2_title]').val();
+	var sImageURL = $('input:hidden[@name=asv_amazon2_sImageURL]').val();
+	var mImageURL = $('input:hidden[@name=asv_amazon2_mImageURL]').val();
+	var lImageURL = $('input:hidden[@name=asv_amazon2_lImageURL]').val();
+	var url = $('input:hidden[@name=asv_amazon2_sImageURL]').val();
+	
+	var line = '';
+	line += "$forms[0]";
+	line = line.replace('[asv_amazon2_asin]', asin);
+	line = line.replace('[asv_amazon2_imgURLs]', sImageURL);
+	line = line.replace('[asv_amazon2_imgURLm]', mImageURL);
+	line = line.replace('[asv_amazon2_imgURLl]', lImageURL);
+	line = line.replace('[asv_amazon2_url]', url);
+	
+	$('#body').appendVal('\\n' + line);
+	
+	
+}
+
+//-------------------------------------------------------------  
+//Add the Amazon information to the body of the article
+
+function asv_onClick(elem)
+{
+	$('#body').append(" a");
+}
+
+//-------------------------------------------------------------
+
+function asv_cancelResults()
+{
+	$('#asv_amazon2Results').hide('slow');
+	$('#asv_amazon2form').hide('slow');
+}
+
+//-------------------------------------------------------------
+
 function asv_prevnext_link(keywords, searchindex, itemPage, totalPages, loc)
 {	
-	line ='';
-	if(parseInt(totalPages)>1){
+	line = '';
+	
+	if(parseInt(totalPages)>1)
+	{
 		if(loc=="bottom") line+='<hr />';
+		
 		line+='<p style="text-align: center;">';
 		
 		itemPage = parseInt(itemPage);
+		
 		var text = '';
-		if(itemPage>1){
+		
+		if(itemPage>1)
+		{
 			var pItemPage = itemPage - 1;
-			line += '<a href="#" onclick="asv_cancelResults(); asv_request(\''+keywords + '\',\'' + searchindex + '\',\''+ pItemPage+'\');return false;">previous</a> | ';
-		}
-		if(itemPage<totalPages){
-			var nItemPage = itemPage + 1;
-			line += '<a href="#" onclick="asv_cancelResults(); asv_request(\''+keywords + '\',\'' + searchindex + '\',\''+ nItemPage+'\');return false;">next</a>';
+			
+			line += '<a href="#" onclick="asv_request(\''+keywords + '\',\'' + searchindex + '\',\''+ pItemPage+'\');return false;">previous</a> | ';
 			
 		}
+		if(itemPage<totalPages)
+		{
+			var nItemPage = itemPage + 1;
+			
+			line += '<a href="#" onclick="asv_request(\''+keywords + '\',\'' + searchindex + '\',\''+ nItemPage+'\');return false;">next</a>';	
+			
+		}
+		
 		line+='</p>';
+		
 		if(loc=="top") line+='<hr />';
 	}
+	
 	return line;
 }
+
+//-------------------------------------------------------------
+
 // -->
 </script>
 EOF;
 	echo $js;
 }
 
+//-------------------------------------------------------------
+
 function asv_safeJS($line)
 {
 	return str_replace("\n", "", addslashes($line));
 }
+
+//-------------------------------------------------------------
 	
 class asv_Amazon
 {
@@ -204,15 +289,19 @@ class asv_Amazon
 	var $version = "2005-03-23";
 	var $locale;
 	
+	//-------------------------------------------------------------
+	
 	function asv_Amazon($locale)
 	{
 		$this->$locale = $locale;
-		
 	}
+	
+	//-------------------------------------------------------------
 	
 	function request($keywords, $searchindex, $itemPage = "1")
 	{
 		$url = $this->buildURL(doSlash($keywords), doSlash($searchindex), doSlash($itemPage));
+
 		// create a new curl resource
 		$ch = curl_init();
 
@@ -224,18 +313,28 @@ class asv_Amazon
 
 		// close curl resource, and free up system resources
 		curl_close($ch);
-
-		//return "not implemented yet";
 	}
+	
+	//-------------------------------------------------------------
 	
 	function buildURL /* private */($keywords, $searchindex, $itemPage = "1")
 	{
-		$url = $this->baseURL."Service=".$this->service."&Operation=".$this->operation."&SubscriptionId=".$this->api_key."&SearchIndex=".$searchindex."&Keywords=".$keywords."&ResponseGroup=".$this->responseGroup."&ItemPage=".$itemPage."&Version=".$this->version;
+		$url = $this->baseURL.
+				"Service=".$this->service.
+				"&Operation=".$this->operation.
+				"&SubscriptionId=".$this->api_key.
+				"&SearchIndex=".$searchindex.
+				"&Keywords=".$keywords.
+				"&ResponseGroup=".$this->responseGroup.
+				"&ItemPage=".$itemPage."&Version=".$this->version;
+		
 		return $url;
 	}
+	
+	//-------------------------------------------------------------
 }
 
+//-------------------------------------------------------------
 
 # --- END PLUGIN CODE ---
-
 ?>
