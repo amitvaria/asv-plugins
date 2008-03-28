c6365570daf2512bab3cdf3b79da9d66a1e6e070
variaas <variaas@c64d0be3-9520-0410-ba41-dbcfa880427b>
2008-03-28 22:22:36 +0000

    updated metadata to reflect v0.1

--- asv_rss_importer/asv_rss_importer.php
+++ asv_rss_importer/asv_rss_importer.php
@@ -8,18 +8,18 @@
 
 // Plugin name is optional.  If unset, it will be extracted from the current
 // file name. Uncomment and edit this line to override:
-$plugin['name'] = '';
+$plugin['name'] = 'asv_rss_importer';
 
-$plugin['version'] = '';
+$plugin['version'] = '0.1';
 $plugin['author'] = 'Amit Varia';
 $plugin['author_uri'] = 'http://www.amitvaria.com/';
-$plugin['description'] = '';
+$plugin['description'] = 'Import RSS feeds in to Textpattern';
 
 // Plugin types:
 // 0 = regular plugin; loaded on the public web side only
 // 1 = admin plugin; loaded on both the public and admin side
 // 2 = library; loaded only when include_plugin() or require_plugin() is called
-$plugin['type'] = 0; 
+$plugin['type'] = 1; 
 
 
 @include_once('../zem_tpl.php');
